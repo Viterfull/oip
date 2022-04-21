89045afa353d4eaf1f4ee5690489a4911d093f97
Viterfull <100037636+Viterfull@users.noreply.github.com>
2022-04-21 15:20:07 +0300

    оип - еип

--- 7-1.py
+++ 7-1.py
@@ -5,111 +5,103 @@ import matplotlib.pyplot as plt
 
 
 GPIO.setmode(GPIO.BCM)   
-
+# массивы с  пинами и нужными для работы данными
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
-cad = [10, 9, 11, 5, 6, 13, 19, 26]
-mas = [0, 0, 0, 0, 0, 0, 0, 0]
 leds = [24, 25, 8, 7, 12, 16, 20, 21]
-led = []
 comp = 4
+arr = [0]*8
 troyka = 17
-datad = []
+datad = [] #сюда сохраняются данные
 
+#настройка пинов
 GPIO.setup(dac, GPIO.OUT)
 GPIO.setup(leds, GPIO.OUT)
 GPIO.setup(troyka, GPIO.OUT)
 GPIO.setup(comp, GPIO.IN)
 
 def decimal2binary(value):
+    '''
+        перевод числа в двоичный код в формате массива
+    '''
     return [int(bit) for bit in bin(value)[2:].zfill(8)]
 
-i = 0
-def adc():
-    
-    for i in 7, 6, 5, 4, 3, 2, 1, 0:
-        #print(i)
-        GPIO.output(cad[i], 1)
-        mas[7 - i] = 1
-        time.sleep(0.007)
-        if(GPIO.input(comp) == 0):
-            GPIO.output(cad[i], 0)
-            mas[7-i] = 0
+def abc():
+    '''
+        взятие значения на тройке
+    '''
+    GPIO.output(dac, 0)
+    for i in range(8):
+        GPIO.output(dac[i], 1)
+        arr[i] = 1
+        time.sleep(0.005)
+        if GPIO.input(comp) == 0:
+            GPIO.output(dac[i], 0)
+            arr[i] = 0
+    arr.reverse()
+    g = 0
+    for i in range(8):
+        g += 2**i*arr[i]
+    return g
 try:
     GPIO.output(troyka, 1)
     v = 0
     volt = 0
-    p = 0
     kolvo = 0
-    start_time = time.time()
+    start_time = time.time()#запись времени
+    #расчеты при зарядке
     while(volt < 3.15):
-        #v = adc()
-        #if(p == 0):     
-            #onesec = time.time()
-        #onesec = time.time()
-        kolvo = kolvo + 1
+        kolvo += 1
         v = 0
-        mas = [0, 0, 0, 0, 0, 0, 0, 0]
-        GPIO.output(dac, mas)
-        adc()
-        for j in range(8):
-            v += mas[j]*(2**(7-j))
+        GPIO.output(dac, decimal2binary(abc()))
+        v = abc()
         volt = v*3.3/256
-        print(v, "  ", volt)
-        datad.append(volt) 
+        print('отсчет' ,v, 'вольты', volt)
+        datad.append(v) #запись данных
+        #зелененькая подсветка 
         GPIO.output(leds, 0)
-        k = 0
         for k in range(9):
             if(v < k * 32 + 5):
                 j = 0
                 for j in range(k):
                     GPIO.output(leds[j], 1)
-                    #print(j)
                 break
-        #if(p == 0):     
-            #twosec = time.time()
-            #p = 1
     GPIO.output(troyka, 0)
     while(volt > 0.1):
-        #v = adc()
         v = 0
-        mas = [0, 0, 0, 0, 0, 0, 0, 0]
         kolvo = kolvo + 1
-        GPIO.output(dac, mas)
-        adc()
-        for j in range(8):
-            v += mas[j]*(2**(7-j))
+        GPIO.output(dac, decimal2binary(abc()))
+        v = abc()
         volt = v*3.3/256
-        print(v, "  ", volt)
-        datad.append(volt) 
+        print('отсчет' ,v, 'вольты', volt)
+        datad.append(v) #запись данных
+        #зелененькая подсветка 
         GPIO.output(leds, 0)
-        k = 0
         for k in range(9):
             if(v < k * 32 + 5):
                 j = 0
                 for j in range(k):
                     GPIO.output(leds[j], 1)
-                    #print(j)
                 break 
+    #расчеты времяни и печать данных
     sec = time.time() - start_time
-    T = float(sec) / kolvo
-    print(kolvo)
+    T = sec / kolvo
     print("--- t = %s seconds ---" % sec)
-    print("--- T = %f seconds ---" % T)
-    print("--- v = %f Hz ---" %  (float(1)/T))
-    print("--- dV = %f volts ---" % float(3.3/256))
+    print("--- T = %.3f seconds ---" % T)
+    print("--- v = %.3f Hz ---" %  (1/T))
+    print("--- dV = %.3f volts ---" % float(3.3/256))
     sets = []
-    sets.append((float(1)/T))
-    sets.append(float(3.3/256))
+    sets.append((1/T))
+    sets.append(3.3/256)
     set_str = [str(i) for i in sets]  
     data_str = [str(item) for item in datad]  
+    #вывод графика
     plt.plot(datad)
     plt.show()
+    #сохранение в файл
     with open("data.txt", "w") as outfile:
-        outfile.write("\n".join(data_str))  
-        #outfile.write("\n".join(sec))
+        outfile.write("\n".join(data_str)) 
     with open("settings.txt", "w") as set:
-        set.write("\n".join(set_str))  
-        #outfile.write("\n".join(sec))
+        set.write("\n".join(set_str))
 finally:
     GPIO.output(dac, 0)
     GPIO.cleanup()
